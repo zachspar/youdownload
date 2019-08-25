9b26c24a107da561eb983ad333e148136e4bf2c7
Zachary Spar <zachspar@gmail.com>
2019-08-25 16:56:25 -0700

    update ephemeral file system

--- youdownload/__init__.py
+++ youdownload/__init__.py
@@ -4,6 +4,16 @@ from flask import Flask
 
 app = Flask(__name__, instance_relative_config=True)
 
+try:
+    os.makedirs(os.path.join(os.getcwd(),
+                             'www', 'yd',
+                             'songs', 'mp3'))
+    os.makedirs(os.path.join(os.getcwd(),
+                             'youdownload',
+                             'static'))
+except OSError as e:
+    print(e)
+
 app.config.from_mapping(SECRET_KEY='dev',
                         TEMPLATE=os.environ.get('YD_OUTTMPL'),
                         CWD=os.getcwd(),

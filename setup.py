40f8dee422e25a9b94c6ba3035da85c50ab594ff
Zachary Spar <zachspar@macbook-pro.lan>
2019-08-24 21:36:17 -0700

    adding starter module files and gitignore

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from setuptools import find_packages, setup
+
+setup(
+    name='youdownload',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+        'youtube_dl',
+    ],
+)
+
---
+++ youdownload/youdownload.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from __future__ import unicode_literals
+import youtube_dl
+
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=F22DCvJ24Eg'])
+

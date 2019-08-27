a72430b74888ed734c57f318d6eb49486d9165a3
Zachary Spar <zachspar@gmail.com>
2019-08-26 18:56:40 -0700

    add config file

---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import os
+
+
+SECRET_KEY='\xa2\xd6OE=w.\xea\x0b\xc6\xde\xebs`H\xa1\x190\xda\xe15\x93\n\xb2'
+STATIC_FOLDER = os.path.join(os.getcwd(),
+                             'youdownload',
+                             'static')
+SONG_FOLDER = os.path.join(os.getcwd(),
+                           'www', 'yd',
+                           'songs', 'mp3')
+
